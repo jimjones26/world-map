d0e762498eddf1fe8222dd5b6a096c128b25e30e
jjones <jimmy.jones@tutanota.com>
2023-10-20 17:59:13 -0600

    add data store and store map data in it, verify context is working

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+	import { dataStore } from '$lib/stores/map-data-store';
+	import { setContext } from 'svelte';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
+
+	$: dataStore.set(data.data);
+
+	setContext('parsedData', dataStore);
 </script>
 
-<div>{JSON.stringify(data.data)}</div>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<div>page</div>
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import type { Writable } from 'svelte/store';
+
+	// grab map data from the data store
+	const chartData: Writable<Array<any>> = getContext('parsedData');
+</script>
+
+{JSON.stringify($chartData)}
