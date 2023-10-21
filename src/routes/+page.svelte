b64ad47289eee9b4aefd2be90d2e7657e0fac3c0
jjones <jimmy.jones@tutanota.com>
2023-10-20 20:06:17 -0600

    create svg container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,4 +4,13 @@
 
 	// grab map data from the data store
 	const chartData: Writable<Array<any>> = getContext('parsedData');
+
+	// width, height and margins for svg container
+	const width: number = 960;
+	const height: number = 500;
+	const margin = { top: 20, right: 30, bottom: 50, left: 70 };
 </script>
+
+<svg {width} {height}>
+	<g transform={`translate(${margin.left}, ${margin.top})`} />
+</svg>
