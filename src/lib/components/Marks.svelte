27367ea02f61b9b0b02b069fab387c1cec36dcc8
jjones <jimmy.jones@tutanota.com>
2023-10-20 20:34:26 -0600

    create map using geojson data

--- src/lib/components/Marks.svelte
+++ src/lib/components/Marks.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+	import * as d3 from 'd3';
 	export let data: any;
 
-	console.log(data);
+	const projection = d3.geoEqualEarth();
+	const path = d3.geoPath(projection);
 </script>
+
+{#each data.features as feature, i}
+	<g>
+		<path d={path({ type: 'Sphere' })} fill="none" stroke="#ccc" />
+		<path d={path(feature)} fill="#ccc" stroke="#fff" />
+	</g>
+{/each}
