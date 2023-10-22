85e880a6ea59f539ed44aa18cd8aac8b916683c0
jjones <jimmy.jones@tutanota.com>
2023-10-21 20:21:05 -0600

    simplify prop, remove unused margin object

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,13 @@
 	import { getContext } from 'svelte';
 
 	// grab map data from the data store
-	const mapData: any = getContext('parsedData');
+	const data: any = getContext('parsedData');
 
 	// width, height and margins for svg container
 	const width: number = 960;
 	const height: number = 500;
-	const margin = { top: 20, right: 30, bottom: 50, left: 70 };
 </script>
 
 <svg {width} {height}>
-	<Marks countries={$mapData.countries} interiors={$mapData.interiors} />
+	<Marks data={$data} />
 </svg>
