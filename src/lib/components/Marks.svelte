9d7485f695a53cd893d5b264b75924d67d3f81b1
jjones <jimmy.jones@tutanota.com>
2023-10-21 20:28:48 -0600

    add graticules, reorder elements, remove sphere from each block

--- src/lib/components/Marks.svelte
+++ src/lib/components/Marks.svelte
@@ -4,11 +4,13 @@
 
 	const projection = d3.geoNaturalEarth1();
 	const path = d3.geoPath(projection);
+	const graticule = d3.geoGraticule();
 </script>
 
+<path d={path({ type: 'Sphere' })} fill="none" stroke="#ccc" />
+<path d={path(graticule())} fill="none" stroke="#efefef" />
 {#each data.land.features as feature, i}
 	<g>
-		<path d={path({ type: 'Sphere' })} fill="none" stroke="#ccc" />
 		<path d={path(feature)} fill="#ccc" stroke="none" />
 	</g>
 {/each}
