518158e6f0e778fb0921aeb57ef9783a52884ebe
jjones <jimmy.jones@tutanota.com>
2023-10-28 20:47:33 -0600

    update marks component to show circle for each city

--- src/lib/components/Marks.svelte
+++ src/lib/components/Marks.svelte
@@ -7,14 +7,21 @@
 	const path = d3.geoPath(projection);
 	const graticule = d3.geoGraticule();
 
-	console.log('FROM MARKS: ', citiesData.cities[0]);
+	const getLngLat = (city: any) => {
+		return projection([city.lng, city.lat]);
+	};
 </script>
 
-<path d={path({ type: 'Sphere' })} fill="none" stroke="#ccc" />
-<path d={path(graticule())} fill="none" stroke="#efefef" />
-{#each worldData.land.features as feature, i}
-	<g>
-		<path d={path(feature)} fill="#ccc" stroke="none" />
-	</g>
-{/each}
-<path d={path(worldData.interiors)} fill="none" stroke="white" />
+<g>
+	<path d={path({ type: 'Sphere' })} fill="none" stroke="#ccc" />
+	<path d={path(graticule())} fill="none" stroke="#efefef" />
+	{#each worldData.land.features as feature, i}
+		<g>
+			<path d={path(feature)} fill="#ccc" stroke="none" />
+		</g>
+	{/each}
+	<path d={path(worldData.interiors)} fill="none" stroke="white" />
+	{#each citiesData.cities as city}
+		<circle cx={getLngLat(city)?.[0]} cy={getLngLat(city)?.[1]} r="1" />
+	{/each}
+</g>
