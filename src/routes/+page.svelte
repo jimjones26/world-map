ca211b976cd6354696faa33c0908d7cdf65d6da8
jjones <jimmy.jones@tutanota.com>
2023-10-28 19:08:21 -0600

    load cities from data store, check with log

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,14 @@
 	// grab map data from the world data store
 	const worldData: any = getContext('worldData');
 
+	// grab cities data from the cities data store
+	const citiesData: any = getContext('citiesData');
+
 	// width, height and margins for svg container
 	const width: number = 960;
 	const height: number = 500;
+
+	console.log('CITIES: ', $citiesData);
 </script>
 
 <svg {width} {height}>
