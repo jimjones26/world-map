44b3fb87bb00bd93329c4873ef5a9398c1faf29e
jjones <jimmy.jones@tutanota.com>
2023-10-28 18:40:18 -0600

    load cities data

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,5 +5,12 @@ export const load: LayoutLoad = async ({ fetch, params }) => {
   const worldAtlasUrl: string = 'https://gist.githubusercontent.com/jimjones26/d8fa5949da7090b7d5be3695db87dbeb/raw/world-map-data.json'
   const worldAtlasRes = await fetch(worldAtlasUrl)
 
-  return { worldData: await worldAtlasRes.json() };
+  // load cities data
+  const citiesUrl: string = 'https://gist.githubusercontent.com/curran/13d30e855d48cdd6f22acdf0afe27286/raw/worldcities_clean.csv'
+  const citiesRes = await fetch(citiesUrl)
+
+  return {
+    worldData: await worldAtlasRes.json(),
+    citiesData: await citiesRes.text()
+  };
 };
