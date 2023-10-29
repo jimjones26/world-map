401ca65110b95e9de4decb38b3df6a569bb12b6b
jjones <jimmy.jones@tutanota.com>
2023-10-28 19:07:42 -0600

    load cities data and set data in citites store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
-	import { worldDataStore } from '$lib/stores/world-data';
+	import type { LayoutData } from './$types';
 	import { setContext } from 'svelte';
 	import * as topojson from 'topojson-client';
-	import type { LayoutData } from './$types';
+	import * as d3 from 'd3';
+
+	import { worldDataStore } from '$lib/stores/world-data';
+	import { citiesDataStore } from '$lib/stores/cities-data';
 
 	export let data: LayoutData;
 
 	const { countries, land } = data.worldData.objects;
+	const cities = data.citiesData;
 
 	$: worldDataStore.set({
 		countries: topojson.feature(data.worldData, countries),
 		interiors: topojson.mesh(data.worldData, countries, (a, b) => a !== b),
 		land: topojson.feature(data.worldData, land)
 	});
+	$: citiesDataStore.set({
+		cities: d3.csvParse(cities, d3.autoType)
+	});
 
 	setContext('worldData', worldDataStore);
+	setContext('citiesData', citiesDataStore);
 </script>
 
 <slot />
